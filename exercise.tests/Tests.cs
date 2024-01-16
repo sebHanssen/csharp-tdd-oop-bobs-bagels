9a45832687d2253d205a324c22bdc789aa25f74b
Sebastian <sebastian.hanssen@outlook.com>
2024-01-16 10:46:27 +0100

    add test RemoveBagel

--- exercise.tests/Tests.cs
+++ exercise.tests/Tests.cs
@@ -15,4 +15,17 @@ public class Tests
         Assert.That(shop.basket.content, Is.Not.Null);
         Assert.That(shop.basket.content[0].Name == "Plain Bagel");
     }
+
+    [Test]
+    public void RemoveBagel()
+    {
+        //Set Up
+        BagelShop shop = new BagelShop();
+        //Execute
+        shop.basket.add(shop.menu.bagels[0]);
+        shop.basket.add(shop.menu.bagels[1]);
+        shop.basket.remove(shop.menu.bagels[0]);
+        //verify
+        Assert.That(shop.basket.content[0].Name == "Onion Bagel");
+    }
 }
